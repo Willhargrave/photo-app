93800b24e0ff600fda2be1d08fff1757919fefbb
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2023-04-14 12:03:32 +0900

    controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
 require 'flickr'
-  def index
 
+
+  def index
+    begin
+      flickr = Flickr.new
+      @photos = flickr.photos.search(user_id: params[:user_id])
+    end
   end
 end
